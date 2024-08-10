e70e3741b1172d6657391e228a2f0490069551ce
Narmin Dilbazi <145699033+NarminDilbazi1@users.noreply.github.com>
2024-08-10 16:06:50 +0400

    Add files via upload

---
+++ izzidesign/index.js
@@ -0,0 +1,42 @@
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        const services = data.data.services;
+        let output = '';
+
+        services.forEach(service => {
+            let subServicesOutput = '';
+            service.subServices.forEach(subService => {
+                subServicesOutput += `<p>${subService.title_en}</p><hr>`;
+            });
+
+            output += `
+                <div class="service">
+                    <div class="serviceMain">
+                        <img src="${service.cover_img.publicUrl}" class="serviceImg">
+                        <div class="serviceInfo">
+                            <p class="serviceName">${service.title_en}</p>
+                            <p class="cost"><img src="img/cost.svg"> Starts from 20$/h</p>
+                        </div>
+                    </div>
+                    <div class="subServices" style="display: none;">
+                        ${subServicesOutput}
+                    </div>
+                </div>
+            `;
+        });
+
+        document.querySelector('.services').innerHTML = output;
+
+        document.querySelectorAll('.service').forEach(serviceDiv => {
+            serviceDiv.addEventListener('click', function() {
+                const subServiceDiv = serviceDiv.querySelector('.subServices');
+                if (subServiceDiv.style.display === 'none' || subServiceDiv.style.display === '') {
+                    subServiceDiv.style.display = 'flex';
+                } else {
+                    subServiceDiv.style.display = 'none';
+                }
+            });
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
